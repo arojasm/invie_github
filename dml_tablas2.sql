e094f283b28c1479ccd90783ece952664c1e365c
Arturo <arrome10@gmail.com>
2018-05-05 04:01:12 -0500

    agregando los campos direccion y sexo en la tabla de datos , e incluyendo en el proyecto el script de inserción

---
+++ dml_tablas2.sql
@@ -0,0 +1,7 @@
+
+-- insertando los valores a la tabla:
+insert into datos 
+(
+nombre ,apellidos ,DNI ,direccion , sexo )
+values 
+("Arturo", "R", "40098767", "Peru", "M");
--- tablas.sql
+++ tablas.sql
@@ -1,6 +1,8 @@
 create table datos (
 nombre varchar2(100),
 apellidos varchar2(100),
-DNI char(8)
+DNI char(8),
+direccion varchar2(200),
+sexo char(1)
 )
 tablespace test nologging
